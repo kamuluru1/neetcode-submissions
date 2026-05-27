8450943239ea15579b8f8beaa9c8841ab360c945
kamuluru1 <148723256+kamuluru1@users.noreply.github.com>
2026-05-27 18:34:08 -0400

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+
+        unordered_map<char, char> p_map = {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}
+        };
+
+        stack<char> s1;
+
+        for (auto c : s) {
+            if (c == '(' || c == '[' || c == '{') {
+                s1.push(c);
+            } else {
+                if (s1.size() == 0) return false;
+
+                if (s1.top() == p_map[c]) {
+                    s1.pop();
+                } else return false;
+            }
+        }
+
+
+        return s1.size() == 0;
+    }
+};
